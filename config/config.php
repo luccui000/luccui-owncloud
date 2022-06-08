712d14bf9e6b49bee87d6b86990488a1282b7953
luccui000 <luccui2k@gmail.com>
2022-06-08 06:32:20 +0000

    first commit

---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oce3p74398g4',
+  'passwordsalt' => 'vFskz+mb+xsSXmMxeyG4cfKZLl7vGM',
+  'secret' => 'TpjXsUk0InQZ5Lm26sJZk/p5zFjz9+5u4uNAnoA/5w3Hu9oW',
+  'trusted_domains' => 
+  array (
+    0 => '172.247.26.206',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://172.247.26.206/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '10.4.0.4',
+  'dbname' => 'ownclound',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'SIsLxxGXAj3qqI2wUd+XwXCldtXrN4',
+  'logtimezone' => 'UTC',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/owncloud/apps-external',
+      'url' => '/apps-external',
+      'writable' => true,
+    ),
+  ),
+  'installed' => true,
+);
---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,4,0,4);
+$OC_VersionString = '10.4.0';
+$OC_Channel = 'stable';
+$OC_Edition = 'community';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2020-02-27T14:31:06+00:00 8b878ef9b40ba850b6ac3bf20250f2923262c0d5';
+$vendor = 'owncloud';
